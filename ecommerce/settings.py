3cd9a89cdb5fa119b5132edd062311d92ee06317
Sravani Cheripally <sravani.cheripally@external-travclan.com>
2025-09-06 17:52:29 +0530

    add allowedhost

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(-fpmz*2^ye#$jvijl+jer29y)by0rjx_1b7^5u-w+@c--f#7$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
